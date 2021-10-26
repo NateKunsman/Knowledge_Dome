aab35d7eeca2db226ebb22eff0222e6a430cd23a
NateKunsman <nkkuns@protonmail.com>
2021-10-26 11:46:30 -0400

    added models for Author

---
+++ Dome.Services/AuthorService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dome.Services
+{
+    public class AuthorService
+    {
+        //CRUD
+        //Create Author
+        //Get Author
+        //Edit Author
+        //Delete Author
+    }
+}
