1fd5d46b863e7d2f7903397d16a35faafe2319d7
CJ <thompsoncj93@gmail.com>
2021-10-26 14:22:25 -0400

    GenreClasses

---
+++ Dome.Services/GenreService.cs
@@ -0,0 +1,52 @@
+﻿using Dome.Data;
+using Dome.Models;
+using Knowledge_Dome.Dome.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dome.Services
+{
+    public class GenreService
+    {
+        private readonly Guid _userId;
+
+        public GenreService(Guid userId)
+        {
+            _userId = userId;
+        }
+        public bool CreateGenre(GenreCreate model)
+        {
+            var entity =
+                new Genre()
+                {
+                    GenreName = model.GenreName 
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Genres.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public IEnumerable<GenreListItem> GetGenres()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Genres
+                        .Select(
+                            e =>
+                                new GenreListItem
+                                {
+                                    GenreId = e.GenreId
+                                }
+                        );
+                return query.ToArray();
+            }
+        }
+    }
+    
+}
---
+++ Knowledge_Dome/Controllers/GenreController.cs
@@ -0,0 +1,46 @@
+﻿using Dome.Data;
+using Dome.Models;
+using Dome.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Knowledge_Dome.Controllers
+{
+    [Authorize]
+    public class GenreController : ApiController
+    {
+        private GenreService CreateGenreService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var genreService = new GenreService(userId);
+            return genreService;
+        }
+
+        public IHttpActionResult Get()
+        {
+            GenreService genreService = CreateGenreService();
+            var genres = genreService.GetGenres();
+            return Ok(genres);
+        }
+
+        public IHttpActionResult Post(GenreCreate genre)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateGenreService();
+
+            if (!service.CreateGenre(genre))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+
+    }
+}
