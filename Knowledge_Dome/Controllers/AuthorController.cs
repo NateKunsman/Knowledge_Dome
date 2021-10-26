c6ad39bd6a2d1d4de7d8ec7e5eabc034a7cb1c3e
NateKunsman <nkkuns@protonmail.com>
2021-10-26 16:08:44 -0400

    added AuthorController stuffs

--- Knowledge_Dome/Controllers/AuthorController.cs
+++ Knowledge_Dome/Controllers/AuthorController.cs
@@ -1,4 +1,6 @@
-﻿using Dome.Services;
+﻿using Dome.Models;
+using Dome.Services;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +17,36 @@ namespace Knowledge_Dome.Controllers
             AuthorService authorService = CreateAuthorService();
             var authors = authorService.GetAuthors();
             return Ok(authors);
+        }
+        public IHttpActionResult Post(AuthorCreate author)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreateAuthorService();
+            if (!service.CreateAuthor(author))
+                return InternalServerError();
+            return Ok();
+        }
+        private AuthorService CreateAuthorService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var noteService = new AuthorService(userId);
+            return noteService;
+        }
+        public IHttpActionResult GetAuthorByName(string name)
+        {
+            AuthorService authorService = CreateAuthorService();
+            var author = authorService.GetAuthorByName(name);
+            return Ok(name);
+        }
+        public IHttpActionResult Put(AuthorEdit author)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreateAuthorService();
+            if (!service.UpdateAuthor (author))
+                return InternalServerError();
+            return Ok();
         }
     }
 }
